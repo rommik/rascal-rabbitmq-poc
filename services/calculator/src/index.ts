ee6c86c53d263e6b21ad65e8353e79ab5765523f
Roman Mikhailov <roman.mikhailov@barco.com>
2020-10-30 00:39:12 -0400

    basic rascal demo

--- services/calculator/src/index.ts
+++ services/calculator/src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello from the Calculator');
+import {BrokerAsPromised as Broker} from 'rascal';
+import config from './config/rascal.json';
+
+(async () => {
+    try {
+      const broker = await Broker.create(config);
+      broker.on('error', console.error);
+  
+      // Publish a message
+      const publication = await broker.publish('demo_publication', 'Hello World!');
+      publication.on('error', console.error);
+  
+      // Consume a message
+      const subscription = await broker.subscribe('demo_subscription');
+      subscription.on('message', (message, content, ackOrNack) => {
+        console.log(content);
+        ackOrNack();
+      }).on('error', console.error);
+    } catch(err) {
+      console.error(err);
+    }
+  })();
